5b5c9caa6989d32838e51a71a9177a50e7336b5c
aikoels <quincyels@gmail.com>
2017-12-08 20:13:02 -0500

    remove trail heads

---
+++ SQL_Files/RemoveData/remove_trail_heads.sql
@@ -0,0 +1,17 @@
+USE hikers_guide_to_the_galaxy;
+
+DROP Procedure IF EXISTS remove_trail_head;
+
+DELIMITER //
+
+CREATE Procedure remove_trail_head (IN gpsID int, IN trailID int)
+BEGIN
+
+    -- Delete the actual trail
+    delete
+    from trail_heads
+    where gpsID = gps_id AND trail_id = trailID;
+
+
+END //
+DELIMITER ;
